cbc24dad0fbc9fbb6f17b44d24bf1e26d381418d
AndrewFelipeCarneiro <160683280+AndrewFelipeCarneiro@users.noreply.github.com>
2024-02-20 22:08:43 -0300

    Create Atividade 3.java

---
+++ Atividade 3.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String nomeDoAluno = "Andrew";
+        int idadeDoAluno = 21;
+        double notaDaProva = 9.5;    
+        double notaDaProva2 = 8.0;
+        double mediaFinal = 8.75;
+        
+        
+        
+        
+        
+    }
+}
