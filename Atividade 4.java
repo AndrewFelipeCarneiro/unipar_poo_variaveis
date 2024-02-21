2dbb8b65f1915ed50a65b3c60ec7cf970f4df965
AndrewFelipeCarneiro <160683280+AndrewFelipeCarneiro@users.noreply.github.com>
2024-02-20 22:18:57 -0300

    Create Atividade 4.java

---
+++ Atividade 4.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String nomeDoProduto;
+        String categoriaDoProduto;
+        double precoUnitario;
+        int quantidadeEmEstoqueDoProduto;
+        
+    
+    }
+}
